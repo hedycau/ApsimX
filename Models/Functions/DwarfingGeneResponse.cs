f832f55c9182d0f11992109487a7d6467d99a344
Di He <di.he@outlook.com>
2026-03-25 22:29:57 +1100

    Assume the coleoptile elongation rate is 80% of the lab measurement in the soil

--- Models/Functions/DwarfingGeneResponse.cs
+++ Models/Functions/DwarfingGeneResponse.cs
@@ -65,13 +65,13 @@ namespace Models.Functions
 
             ColeoptileElongationRate = DwarfingGeneType switch
             {
-                DwarfingGenesOption.rht => 1.56,
-                DwarfingGenesOption.Rht8 => 1.63,
-                DwarfingGenesOption.Rht13 => 1.63,
-                DwarfingGenesOption.Rht18 => 1.53,
-                DwarfingGenesOption.Rht1 => 1.07,
-                DwarfingGenesOption.Rht2 => 1.05,
-                DwarfingGenesOption.Rht1_Rht2 => 0.908,
+                DwarfingGenesOption.rht => 1.248 ,// 1.56,
+                DwarfingGenesOption.Rht8 => 1.304, //1.63,
+                DwarfingGenesOption.Rht13 => 1.304, //1.63,
+                DwarfingGenesOption.Rht18 => 1.224, //1.53,
+                DwarfingGenesOption.Rht1 => 0.856, //1.07,
+                DwarfingGenesOption.Rht2 => 0.84, //1.05,
+                DwarfingGenesOption.Rht1_Rht2 => 0.7264, //0.908,
                 _ => throw new ArgumentException($"Unsupported genetic type: {DwarfingGeneType}")
             };
 
